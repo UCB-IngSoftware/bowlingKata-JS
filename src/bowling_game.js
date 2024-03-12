27ff89b27c59708f9fb87f0afb41fc3b0226c242
Israel Antezana Rojas <israelantezana@gmail.com>
2024-03-12 00:41:02 -0400

    Refactoring, rename variable frameIndex

--- src/bowling_game.js
+++ src/bowling_game.js
@@ -9,15 +9,15 @@ class BowlingGame {
   }
   score() {
     let result = 0;
-    let i = 0;
+    let frameIndex = 0;
     for (let frame = 0; frame < 10; frame++) {
-      if (this.rolls[i] + this.rolls[i + 1] === 10) {
+      if (this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10) {
         //spare
-        result += 10 + this.rolls[i + 2];
-        i += 2;
+        result += 10 + this.rolls[frameIndex + 2];
+        frameIndex += 2;
       } else {
-        result += this.rolls[i] + this.rolls[i + 1];
-        i += 2;
+        result += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
+        frameIndex += 2;
       }
     }
     return result;
