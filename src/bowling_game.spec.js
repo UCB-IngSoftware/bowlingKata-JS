e31fdc6122a5588898c46262a486a29fd20fcf1e
Israel Antezana Rojas <israelantezana@gmail.com>
2024-03-11 23:58:09 -0400

    Refactoring: Eliminando duplicidad en pruebas con metodo rollMany

--- src/bowling_game.spec.js
+++ src/bowling_game.spec.js
@@ -1,19 +1,19 @@
 import BowlingGame from "./bowling_game.js";
 
 describe("Bowling Game", () => {
-  it("all rolls 0", () => {
-    let game = new BowlingGame();
+  let game = new BowlingGame();
+  function rollMany(pins) {
     for (let i = 0; i < 20; i++) {
-      game.roll(0);
+      game.roll(pins);
     }
+  }
+  it("all rolls 0", () => {
+    rollMany(0);
     expect(game.score()).toEqual(0);
   });
 
   it("all rolls 1", () => {
-    let game = new BowlingGame();
-    for (let i = 0; i < 20; i++) {
-      game.roll(1);
-    }
+    rollMany(1);
     expect(game.score()).toEqual(20);
   });
 });
